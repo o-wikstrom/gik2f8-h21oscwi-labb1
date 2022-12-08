1a031a947f7d6ffe17b629701034d0d80d1fc475
Oscar Wikström <oscar.wikstrom@hotmail.com>
2022-12-08 13:51:31 +0100

    la till HTML för lista, JS för viss logik

---
+++ script.js
@@ -0,0 +1,53 @@
+"use strict";
+/*  Vad ska vi göra här? */
+const bookList = [
+    {
+        id: 1,
+        author: "Charles dickens",
+        title: "Oliver twist",
+    },
+    {
+        id: 2,
+        author: "William Shakespear",
+        title: "Hamlet",
+    },
+];
+
+const searchInput = null;
+
+function handleKeyPress(input) {
+    /* Ta emot/läsa av värdet i inputfältet.
+          Sicka värdet till searchBooks  
+          searchBooks returnerar en filtrerad lista
+          filtrerade listan skickas till renderBookList
+      */
+    searchBooks(input);
+}
+
+function searchBooks(searchTerm) {
+    /*  Loopa igenom bookList
+          För varje varn i loppen, ta det aktuella elementet (boken)
+          jämnför titeln med söktermen
+          Om söktermen finns någonstans i titeln, lägg till elementet i ny lista (filteredList)
+          Returnerar filteredList eller anropar renderBookList?
+  
+  
+      */
+          const filteredList = [];
+    for (let i = 0; i < bookList.length; i++) {
+        const title = bookList[i].title.toLowerCase();
+        
+        if (title.indexOf(searchTerm.toLowerCase()) >= 0) {
+            filteredList.push(bookList[0]);
+        }
+    }
+    renderBookList(filteredList);
+
+
+}
+
+handleKeyPress('e')
+
+function renderBookList(list) {
+    console.log(list);
+ }
