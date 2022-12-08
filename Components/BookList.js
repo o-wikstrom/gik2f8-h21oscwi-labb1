3f7086b315c1a3db20a6a13d5ae7837a3854de12
Oscar Wikström <oscar.wikstrom@hotmail.com>
2022-12-08 15:47:48 +0100

    Komponenter

---
+++ Components/BookList.js
@@ -0,0 +1,12 @@
+const BookList = (bookList) => {
+    console.log(bookList)
+
+    let html = `<ul class="book-list rounded-md border-2 border-blue-400 bg-white w-full mx-auto">`;
+    for (let i = 0; i < bookList.length; i++) {
+        html += BookListItem(bookList[i]);
+
+    }
+    html += `</ul>`;
+
+    return html;
+}
--- script.js
+++ script.js
@@ -51,24 +51,17 @@ function searchBooks(searchTerm) {
 }
 
 function renderBookList(bookList) {
-    let html = `<ul class="book-list rounded-md border-2 border-blue-400 bg-white w-full mx-auto">`;
-    for (let i = 0; i < bookList.length; i++) {
-        html += `<li
-        class="book-list__item mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b border-indigo-700 cursor-pointer">
-        ${bookList[i].author} - ${bookList[i].title}
-        </li>`
-
-    }
-    html += `</ul>`;
-
-    const existingElement = document.querySelector(".book-list");
-    console.log(existingElement);
     
+
+    const existingElement = document.querySelector('.book-list');
+    console.log(existingElement);
+
     const root = document.getElementById('root');
     if (existingElement) {
         root.removeChild(existingElement);
     }
-
-    root.insertAdjacentHTML('beforeend', html);
+    if(bookList.length > 0) {
+        root.insertAdjacentHTML('beforeend', BookList(bookList));
+    }
 
 }
